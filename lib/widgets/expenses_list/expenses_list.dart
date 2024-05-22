45ff92c72bd973d3b62f69d96616a2896b5a2b1d
Luca Faccio <luca@sunnyday.software>
2024-05-22 22:14:23 +0200

    Lesson 126 - Using ThemeData

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -13,14 +13,19 @@ class ExpensesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) =>
-          Dismissible(
-            key: ValueKey(expenses[index]),
-            onDismissed: (direction){
-              onRemoveExpense(expenses[index]);
-            },
-            child: ExpenseItem(expenses[index]),
+      itemBuilder: (context, index) => Dismissible(
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(.75),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
           ),
+        ),
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(expenses[index]),
+      ),
     );
   }
 }
