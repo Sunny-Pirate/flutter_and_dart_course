01de63b6c050a336b8fcd3090ec6c59c177f9ce2
Luca Faccio <luca@sunnyday.software>
2024-05-28 17:17:55 +0200

    Lesson 218 - Polished local version

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -24,29 +24,50 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items added yet'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+          itemCount: _groceryItems.length,
+          itemBuilder: (ctx, index) => Dismissible(
+                key: ValueKey(_groceryItems[index].id),
+                onDismissed: (direction) {
+                  _removeItem(_groceryItems[index]);
+                },
+            secondaryBackground: Container(
+              color: ,
+            ),
+                background: Container(
+                  color: Colors.red,
+                ),
+                child: ListTile(
+                  title: Text(_groceryItems[index].name),
+                  leading: Container(
+                    width: 24,
+                    height: 24,
+                    color: _groceryItems[index].category.color,
+                  ),
+                  trailing: Text(_groceryItems[index].quantity.toString()),
+                ),
+              ));
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: _groceryItems.length == 0
-          ? Center(
-              child: Text('There\'s no items'),
-            )
-          : ListView.builder(
-              itemCount: _groceryItems.length,
-              itemBuilder: (ctx, index) => ListTile(
-                    title: Text(_groceryItems[index].name),
-                    leading: Container(
-                      width: 24,
-                      height: 24,
-                      color: _groceryItems[index].category.color,
-                    ),
-                    trailing: Text(_groceryItems[index].quantity.toString()),
-                  )),
+      body: content,
     );
   }
 }
