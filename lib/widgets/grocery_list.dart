b586a0514f6041b7db47cb1ad23267e674180300
Luca Faccio <luca@sunnyday.software>
2024-05-28 20:06:24 +0200

    Lesson 234 - Future builder introduction. It broke the app.

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -15,63 +15,55 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
-  bool _isLoading = true;
+
+  // bool _isLoading = true;
+  late Future<List<GroceryItem>> _loadedItems;
   String? _error;
 
   @override
   void initState() {
     super.initState();
-    _loadItems();
+    _loadedItems = _loadItems();
   }
 
-  void _loadItems() async {
+  Future<List<GroceryItem>> _loadItems() async {
     final url = Uri.https(
       'flutter-prep-f3b36-default-rtdb.europe-west1.firebasedatabase.app',
       '/shopping-list.json',
     );
 
-    try {
-      final response = await http.get(url);
+    final response = await http.get(url);
 
-      if (response.statusCode >= 400) {
-        setState(() {
-          _error = 'Failed to fetch data. Please try again later.';
-        });
-      }
-
-      // print(response.body);
-      // Implementation of error handling is dependant to the backend you're using. This example is with Firebase
-      if (response.body == 'null') {
-        setState(() {
-          _isLoading = false;
-        });
-        return;
-      }
-      final Map<String, dynamic> listData = json.decode(response.body);
-      final List<GroceryItem> loadedItems = [];
-      for (final item in listData.entries) {
-        final category = categories.entries
-            .firstWhere(
-                (element) => element.value.label == item.value['category'])
-            .value;
-        loadedItems.add(GroceryItem(
-          id: item.key,
-          name: item.value['name'],
-          quantity: item.value['quantity'],
-          category: category,
-        ));
-      }
-      setState(() {
-        _groceryItems = loadedItems;
-        _isLoading = false;
-      });
-    } catch (e) {
-      setState(() {
-        _error = 'Something went wrong. Please try again later.';
-      });
-      throw Exception();
+    if (response.statusCode >= 400) {
+      // setState(() {
+      //   _error = 'Failed to fetch data. Please try again later.';
+      // });
+      throw Exception('Failed to fecth groceries item. Please try again.');
     }
 
+    // print(response.body);
+    // Implementation of error handling is dependant to the backend you're using. This example is with Firebase
+    if (response.body == 'null') {
+      // setState(() {
+      //   _isLoading = false;
+      // });
+      return [];
+    }
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (element) => element.value.label == item.value['category'])
+          .value;
+      loadedItems.add(GroceryItem(
+        id: item.key,
+        name: item.value['name'],
+        quantity: item.value['quantity'],
+        category: category,
+      ));
+    }
+    return loadedItems;
   }
 
   void _addItem() async {
@@ -105,47 +97,47 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
-    Widget content = const Center(
-      child: Text('No items added yet'),
-    );
-
-    if (_isLoading) {
-      content = const Center(child: CircularProgressIndicator());
-    }
-
-    if (_groceryItems.isNotEmpty) {
-      content = ListView.builder(
-          itemCount: _groceryItems.length,
-          itemBuilder: (ctx, index) => Dismissible(
-                key: ValueKey(_groceryItems[index].id),
-                onDismissed: (direction) {
-                  _removeItem(_groceryItems[index]);
-                },
-                background: Container(
-                  color: Colors.red,
-                ),
-                child: ListTile(
-                  title: Text(_groceryItems[index].name),
-                  leading: Container(
-                    width: 24,
-                    height: 24,
-                    color: _groceryItems[index].category.color,
-                  ),
-                  trailing: Text(_groceryItems[index].quantity.toString()),
-                ),
-              ));
-    }
-
-    if (_error != null) {
-      content = Center(child: Text(_error!));
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: content,
+      body: FutureBuilder(
+          future: _loadedItems,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              const Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            }
+            if (snapshot.data!.isEmpty) {
+              const Center(
+                child: Text('No items added yet'),
+              );
+            }
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (ctx, index) => Dismissible(
+                      key: ValueKey(snapshot.data![index].id),
+                      onDismissed: (direction) {
+                        _removeItem(snapshot.data![index]);
+                      },
+                      background: Container(
+                        color: Colors.red,
+                      ),
+                      child: ListTile(
+                        title: Text(snapshot.data![index].name),
+                        leading: Container(
+                          width: 24,
+                          height: 24,
+                          color: snapshot.data![index].category.color,
+                        ),
+                        trailing:
+                            Text(snapshot.data![index].quantity.toString()),
+                      ),
+                    ));
+          }),
     );
   }
 }
