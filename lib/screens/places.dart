5f8483484e341ff20750cc4a936a0d42d4fedaa1
Luca Faccio <luca@sunnyday.software>
2024-06-01 18:37:24 +0200

    Lesson 267 - Store data in a device db

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -5,11 +5,25 @@ import 'package:flutter_and_dart_course/screens/add_place.dart';
 import 'package:flutter_and_dart_course/widgets/places_list.dart';
 import 'package:flutter_and_dart_course/providers/user_places.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<PlacesScreen> createState() => _PlacesScreenState();
+
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userPlaces = ref.watch(userPlacesProvider);
 
     return Scaffold(
@@ -28,9 +42,19 @@ class PlacesScreen extends ConsumerWidget {
           ),
         ],
       ),
-      body: PlacesList(
-        places: userPlaces,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: _placesFuture,
+          builder: (context, snapshot) =>
+          snapshot.connectionState == ConnectionState.waiting
+              ? const Center(child: CircularProgressIndicator())
+              : PlacesList(
+            places: userPlaces,
+          ),
+        ),
       ),
     );
   }
+
 }
