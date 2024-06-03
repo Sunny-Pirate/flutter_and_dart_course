fc013a6793c28d4266ed6c3e50e6bf93f66d23a8
Luca Faccio <luca@sunnyday.software>
2024-06-03 12:17:22 +0200

    Lesson 299 - working with Notification topics

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_and_dart_course/widgets/chat_messages.dart';
 import 'package:flutter_and_dart_course/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotification();
+  }
 
   void _logout() {
     FirebaseAuth.instance.signOut();
@@ -20,7 +40,10 @@ class ChatScreen extends StatelessWidget {
               onPressed: _logout,
               icon: Icon(
                 Icons.exit_to_app,
-                color: Theme.of(context).colorScheme.primary,
+                color: Theme
+                    .of(context)
+                    .colorScheme
+                    .primary,
               ))
         ],
       ),
@@ -32,4 +55,5 @@ class ChatScreen extends StatelessWidget {
       ),
     );
   }
+
 }
